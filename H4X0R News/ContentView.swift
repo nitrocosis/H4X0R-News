e67c403c94c72b2e0da9efce90351632cb0e27a7
Sarah Estrellado <Sarah.estrellado@icloud.com>
2020-08-07 16:54:44 -0500

    Initial commit

---
+++ H4X0R News/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  H4X0R News
+//
+//  Created by Sarah Rebecca Estrellado on 8/7/20.
+//  Copyright © 2020 Sarah Rebecca Estrellado. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
