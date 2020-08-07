219b07adee39e2c3477e797067173bd6788f1d8f
Sarah Estrellado <Sarah.estrellado@icloud.com>
2020-08-07 17:17:12 -0500

    Added list view and navigation view, populated list with posts array placeholder data

--- H4X0R News/ContentView.swift
+++ H4X0R News/ContentView.swift
@@ -10,8 +10,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        
+        NavigationView {
+            List(posts) { post in
+                Text(post.title)
+                
+            }
+                
+            .navigationBarTitle("H4X0R NEWS")
+        }
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
@@ -19,3 +28,14 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+struct Post: Identifiable {
+    let id: String
+    let title: String
+}
+
+let posts = [
+    Post(id: "1", title: "Hello"),
+    Post(id: "2", title: "Bonjour"),
+    Post(id: "3", title: "Hola"),
+]
